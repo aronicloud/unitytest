a22e415f6f15b2c5bf3e0ae62983298ddb615183
aron <arona12@ru.is>
2015-12-06 15:33:31 +0000

    first travis test

---
+++ JustATest/Assets/Editor/BuildScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using System.Linq;
+
+public class BuildScript
+{
+	static void Android()
+	{
+		BuildPipeline.BuildPlayer(GetScenes(), "game.apk", BuildTarget.Android, BuildOptions.None);
+	}
+
+	static string[] GetScenes()
+	{
+		return EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+	}
+}
