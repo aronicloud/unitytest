654f09a48123c80f8826d6de3fa7b2be9c912cf0
aron <arona12@ru.is>
2015-12-06 16:29:24 +0000

    take 11

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -8,7 +8,8 @@
 #curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
 
 echo 'Installing Unity.pkg'
-pwd /
-ls -al /
-ls -al /Applications/Utilities/ /
+pwd
+sw_vers -productVersion
+ls -al
+ls /
 # sudo installer -dumplog -package Unity.pkg -target /
